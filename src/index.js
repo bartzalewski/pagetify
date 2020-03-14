abcfc326f519535710a3bc8d18f2a634ce09ccd2
Bart <adv1key@o2.pl>
2020-03-14 03:14:10 +0100

    Add Firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import fbConfig from './config/fbConfig';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
