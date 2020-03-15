267eb61d3a73c6e07edf3cdf506b53a22b5b97f2
Bart <adv1key@o2.pl>
2020-03-15 02:39:19 +0100

    Change PostList title hover color

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -3,7 +3,23 @@ import styled from 'styled-components';
 import PostSummary from './PostSummary';
 import { Link } from 'react-router-dom';
 
-const StyledPostList = styled.div``;
+const StyledPostList = styled.div`
+	.post__item {
+		transition: 0.2s;
+
+		&:hover {
+			transition: 0.2s;
+
+			.blog__title {
+				background: -webkit-linear-gradient(left, #36d1dc, #5b86e5);
+				-webkit-background-clip: text;
+				background-clip: text;
+				-webkit-text-fill-color: transparent;
+				transition: 0.2s;
+			}
+		}
+	}
+`;
 
 export default function PostList({ posts }) {
 	return (
@@ -11,7 +27,7 @@ export default function PostList({ posts }) {
 			{posts &&
 				posts.map(post => {
 					return (
-						<Link to={'/post/' + post.id}>
+						<Link to={'/post/' + post.id} className="post__item">
 							<PostSummary post={post} key={post.id} />
 						</Link>
 					);
