45814fdf378ac3110fa58a1501d7a51355b65d3c
Bart <adv1key@o2.pl>
2020-03-17 16:32:06 +0100

    Add Form to Price site

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import NavbarOtherSites from '../components/Navbar/NavbarOtherSites';
+import Form from '../components/Form/Form';
 
 const StyledPrice = styled.section``;
 
@@ -15,14 +16,7 @@ export default function Price() {
 				<div className="sites__container">
 					<div className="container">
 						<h1 className="section__title">Price</h1>
-						<div className="sites__wrapper">
-							<p className="sites__desc">
-								Supported mobile devices, easy to use independently, SEO
-								friendly and ready for any expansion at any time. We create
-								websites ideally suited to the needs of both you and your
-								recipients. Check below what features they have.
-							</p>
-						</div>
+						<Form></Form>
 					</div>
 				</div>
 			</StyledPrice>
